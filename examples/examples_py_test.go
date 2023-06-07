371ea70f6d4dcc9cba28a2472e3ed2c13a13a2cb
Lee Briggs <lee@leebriggs.co.uk>
2023-06-07 11:27:13 -0700

    remove python test

--- examples/examples_py_test.go
+++ examples/examples_py_test.go
@@ -3,32 +3,32 @@
 
 package examples
 
-import (
-	"path"
-	"path/filepath"
-	"testing"
+// import (
+// 	"path"
+// 	"path/filepath"
+// 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
-)
+// 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
+// )
 
-func TestBasic(t *testing.T) {
-	test := getPythonBaseOptions(t).
-		With(integration.ProgramTestOptions{
-			Dir: path.Join(getCwd(t), "python"),
-		})
+// func TestBasic(t *testing.T) {
+// 	test := getPythonBaseOptions(t).
+// 		With(integration.ProgramTestOptions{
+// 			Dir: path.Join(getCwd(t), "python"),
+// 		})
 
-	integration.ProgramTest(t, &test)
-}
+// 	integration.ProgramTest(t, &test)
+// }
 
 
-func getPythonBaseOptions(t *testing.T) integration.ProgramTestOptions {
-	base := getBaseOptions(t)
-	basePython := base.With(integration.ProgramTestOptions{
-		ExpectRefreshChanges: true,
-		Dependencies: []string{
-			filepath.Join("..", "sdk", "python", "bin"),
-		},
-	})
+// func getPythonBaseOptions(t *testing.T) integration.ProgramTestOptions {
+// 	base := getBaseOptions(t)
+// 	basePython := base.With(integration.ProgramTestOptions{
+// 		ExpectRefreshChanges: true,
+// 		Dependencies: []string{
+// 			filepath.Join("..", "sdk", "python", "bin"),
+// 		},
+// 	})
 
-	return basePython
-}
+// 	return basePython
+// }
