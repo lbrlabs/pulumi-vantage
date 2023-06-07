106ff87e34878b6661c93c02ec39d05a50341bc1
Lee Briggs <lee@leebriggs.co.uk>
2023-06-07 06:59:02 -0700

    implement tests

--- examples/examples_nodejs_test.go
+++ examples/examples_nodejs_test.go
@@ -4,17 +4,27 @@
 package examples
 
 import (
-	"path/filepath"
+	"path"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
 )
 
+func TestAccServiceAccount(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "typescript"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
 func getJSBaseOptions(t *testing.T) integration.ProgramTestOptions {
-	base := getBaseOptions()
+	base := getBaseOptions(t)
 	baseJS := base.With(integration.ProgramTestOptions{
+		ExpectRefreshChanges: true,
 		Dependencies: []string{
-			"@pulumi/foo",
+			"@lbrlabs/pulumi-grafana",
 		},
 	})
 
--- examples/examples_test.go
+++ examples/examples_test.go
@@ -1,3 +1,5 @@
+// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+
 package examples
 
 import (
@@ -6,6 +8,15 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
 )
+
+func getToken(t *testing.T) string {
+	name := os.Getenv("VANTAGE_API_TOKEN")
+	if name == "" {
+		t.Skipf("Skipping test due to missing VANTAGE_API_TOKEN environment variable")
+	}
+
+	return name
+}
 
 func getCwd(t *testing.T) string {
 	cwd, err := os.Getwd()
@@ -16,9 +27,11 @@ func getCwd(t *testing.T) string {
 	return cwd
 }
 
-func getBaseOptions() integration.ProgramTestOptions {
+func getBaseOptions(t *testing.T) integration.ProgramTestOptions {
+	token := getToken(t)
 	return integration.ProgramTestOptions{
-		RunUpdateTest:        false,
-		ExpectRefreshChanges: true,
+		Config: map[string]string{
+			"vantage_api_token":  token,
+		},
 	}
 }
